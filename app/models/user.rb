e98111c497c41f38368cf5e833c1dea19f28031c
sachin nandha sabarish <sachinjeevan2506@gmail.com>
2018-11-28 23:52:47 +0530

    fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :trackable,
          :recoverable, :rememberable, :validatable, :omniauthable
-         
-        def self.create_with_omniauth(auth)
-   create! do |user|
-     user.provider = auth['provider']
-     user.uid = auth['uid']
-     if auth['info']
-       user.name = auth['info']['name'] || ""
-       user.email = auth['info']['email'] || ""
-     end
-   end
- end
+         def self.new_with_session(params, session)
+          super.tap do |user|
+            if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+              user.email = data["email"] if user.email.blank?
+            end
+          end
+        end
 
-def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
+        def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+          data = access_token.extra.raw_info
+          if user = User.where(:email => data.email).first
+            user.skip_confirmation!
+            user
+          else # Create a user with a stub password. 
+            user = User.new(:email => data.email, :password => Devise.friendly_token[0,20])
+            user.skip_confirmation!
+            user.save! 
+          end
+end
 end
